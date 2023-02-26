0dd60b5a25edb521de9d580ec3aa891bf0910d07
Sanyam Singh <sanyamsingh@Sanyams-MacBook-Air.local>
2023-02-26 09:17:10 +0530

    3000

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:3001", ""],
+    origin: ["http://localhost:3000", ""],
   })
 );
 app.get("/load", async (req, res) => {
